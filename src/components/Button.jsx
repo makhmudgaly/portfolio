d07c4dfe1e2ea73282900a2e85a8faafe20331a5
Makhmud Mustafabek Galy <makhmudgaly2@gmail.com>
2025-11-10 00:13:48 +0700

    feat(section): added Hero Section
    Added Hero section with 3d models and text animations

---
+++ src/components/Button.jsx
@@ -0,0 +1,31 @@
+const Button = ({ text, className, id }) => {
+  return (
+    <a
+      onClick={(e) => {
+        e.preventDefault();
+
+        const target = document.getElementById('counter');
+
+        if (target && id) {
+          const offset = window.innerHeight * 0.15;
+
+          const top =
+            target.getBoundingClientRect().top + window.scrollY - offset;
+
+          window.scrollTo({ top, behavior: 'smooth' });
+        }
+      }}
+      className={`${className ?? ''} cta-wrapper`}
+    >
+      <div className="cta-button group">
+        <div className="bg-circle"></div>
+        <p className="text"> See my Work</p>
+        <div className="arrow-wrapper">
+          <img src="/images/arrow-down.svg" alt="arrow"></img>
+        </div>
+      </div>
+    </a>
+  );
+};
+
+export default Button;
